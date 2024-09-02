6bc95bfb03fa189d4363b6d0443586ab1f761825
cerlo <cerlo.casa@gmail.com>
2024-09-02 17:21:00 +0200

    VERSION 1.0

---
+++ Better tprint.lua
@@ -0,0 +1,71 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Better tprint
+--- MOD_ID: TPRINT
+--- MOD_AUTHOR: [Cerlo]
+--- MOD_DESCRIPTION: This mod improves the tprint function for developers
+--- BADGE_COLOR: 5E993E
+--- PREFIX: TPRINT
+--- LOADER_VERSION_GEQ: 1.0.0
+--- VERSION: 1.0
+
+function tprint(element, indent)
+    if type(element) == 'nil' then
+        return 'ELEMENT IS NIL'
+    end
+    if type(element) == 'number' then
+        return tostring(element)
+    elseif type(element) == 'string' then
+        return element
+    elseif type(element) == 'boolean' then
+        if element then
+            return 'true'
+        else
+            return 'false'
+        end
+    elseif type(element) == 'function' then
+        return 'function'
+    end
+    -- UTILITY
+    local table = element
+    local function FormatK(element)
+        if type(element) == 'number' then
+            return "["..tostring(element).."]"
+        elseif type(element) == 'string' then
+            return "\""..element.."\""
+        end
+    end
+    local function FormatV(element, indent)
+        if type(element) == 'number' then
+            return tostring(element)
+        elseif type(element) == 'string' then
+            return "\""..element.."\""
+        elseif type(element) == 'boolean' then
+            if element then
+                return 'true'
+            else
+                return 'false'
+            end
+        elseif type(element) == 'function' then
+            return 'function'
+        elseif type(element) == 'table' then
+            return tprint(element, indent+1)
+        end
+    end
+    -- CHECK ERRORS
+    if not indent then
+        indent = 0
+    end
+    local PRINT = "{\r\n"
+    indent = indent + 2
+    for k, v in pairs(table) do
+        -- PRINT K
+        PRINT = PRINT .. string.rep(" ", indent) .. FormatK(k) .. " = "
+        -- PRINT V
+        PRINT = PRINT .. FormatV(v, indent)
+        -- NEW LINE
+        PRINT = PRINT .. ",\r\n"
+    end
+    indent = indent - 2
+    PRINT = PRINT .. string.rep(" ", indent) .. "}"
+    return PRINT
+end
---
+++ README.md
@@ -0,0 +1,79 @@
+# Better tprint
+Do you want to print your table but you are tired of the `tprint()` function and how it is junky as hell?  
+I mean look at this:  
+suppose you have this table
+```lua
+local POKEMON = {
+    Types = {
+        [1] = 'Grass',
+        [2] = 'Poison'
+    },
+    Level = 5,
+    HP = {
+        Max = 15,
+        Current = 15
+    },
+    EvolvesTo = {
+        [1] = {
+            Name = 'Ivysaur',
+            LevelRequired = 16,
+            TradeRequired = false,
+            ItemRequired = false
+        }
+    },
+}
+```
+and you want it printed on the console. If you use the vanilla `tprint(POKEMON)`, the result is this
+```lua
+{
+  EvolvesTo= table: 0x63c208e64860   {
+     [1] = table: 0x63c208e648a0      {
+        TradeRequired= "false",
+        ItemRequired= "false",
+        LevelRequired= 16,
+        Name= "Ivysaur",
+      },
+   },
+  Level= 5,
+  Types= table: 0x63c208e64760   {
+     [1] = "Grass",
+     [2] = "Poison",
+   },
+  HP= table: 0x63c208e647e0   {
+     Max= 15,
+     Current= 15,
+   },
+}
+```
+but if you use the **Better tprint** function, the result looks like this:
+```lua
+{
+  "EvolvesTo" = {
+     [1] = {
+        "TradeRequired" = false,
+        "ItemRequired" = false,
+        "LevelRequired" = 16,
+        "Name" = "Ivysaur",
+      },
+   },
+  "Level" = 5,
+  "Types" = {
+     [1] = "Grass",
+     [2] = "Poison",
+   },
+  "HP" = {
+     "Max" = 15,
+     "Current" = 15,
+   },
+}
+```
+The **Better tprint** is designed to handle:
+- tables,
+- numbers,
+- strings,
+- booleans,
+- functions
+in a nice clean way so that you can tell them apart from each other.  
+## How to use
+Download this mod and put it into your *Mods* folder, then you can use `tprint(element)` like you did before and it will show the improved result.  
+You are not limited to passing a table inside the function, it can print all types of variable that lua supports.
